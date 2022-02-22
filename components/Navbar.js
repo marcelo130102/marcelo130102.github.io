70b1a545bd585672fb75a68016e6ba44040c582b
Marcelo Surco <marcelo.surco@utec.edu.pe>
2022-02-22 18:40:02 -0500

    v0.3.1

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,10 +7,7 @@ const Navbar = () => (
         <a className="navbar-brand">Mi portafolio</a>
       </Link>
       
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
+      <div className="collapse navbar-collapse show" id="navbarNav">
         <ul className="navbar-nav ms-auto">
           <li className="nav-item">
             <Link href="/blog">
